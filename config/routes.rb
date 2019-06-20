5278b37ddd18b7dfeeedd8fc477f303508a75e20
Axel Resnik <axelresnik@gmail.com>
2019-06-20 11:30:13 +0300

    resetting git

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root to: 'cocktails#index'
+  resources :cocktails, only: [:index, :new, :create, :show, :destroy] do
+    resources :doses, only: [:new, :create]
+  end
+
+  resources :doses, only: :destroy
+end
